85e71ab0f50806430377cd7e2d98b5baaec81b83
maniamadrid <maniamadrid@users.noreply.github.com>
2026-02-15 22:34:36 +0700

    metas

--- src/main.jsx
+++ src/main.jsx
@@ -3,6 +3,23 @@ import { createRoot } from 'react-dom/client'
 import './index.css'
 import App from './App.jsx'
 
+const GA_MEASUREMENT_ID = import.meta.env.VITE_GA_MEASUREMENT_ID
+const shouldEnableGA = !import.meta.env.DEV && !!GA_MEASUREMENT_ID
+
+if (shouldEnableGA) {
+  const gtagScript = document.createElement('script')
+  gtagScript.async = true
+  gtagScript.src = `https://www.googletagmanager.com/gtag/js?id=${GA_MEASUREMENT_ID}`
+  document.head.appendChild(gtagScript)
+
+  window.dataLayer = window.dataLayer || []
+  window.gtag = function gtag() {
+    window.dataLayer.push(arguments)
+  }
+  window.gtag('js', new Date())
+  window.gtag('config', GA_MEASUREMENT_ID)
+}
+
 createRoot(document.getElementById('root')).render(
   <StrictMode>
     <App />
